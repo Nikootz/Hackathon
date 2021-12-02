c90cedd0c3cbf9354f8287d2a718289e8b1c5a07
nikootz <nicholas.a.gonz@gmail.com>
2021-12-02 00:04:17 -0500

    lastest button

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// let color = ["#e8453c", "#e08b0b", "#f9f62d", "#3aa757", "#6dedb3", "#65d9db", "#63b7f7", "#4688f1", "#e38de0", "#b66ded"];
+let color = "#e8453c";
 
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.storage.sync.set({ color });
-    // console.log('Default background color set to %cpink', `color: ${color}`);
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cpink', `color: ${color}`);
 
-// });
+});
